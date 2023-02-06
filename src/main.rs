5d35354d2b50a5664e256775de882dbc655a96c3
Daniel Stroh <strohdaniel624@gmail.com>
2023-02-06 13:59:51 -0700

    More skeleton work on login

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,17 @@ use axum::{
     http::StatusCode,
     response::{Html, IntoResponse},
     routing::get,
-    Router,
+    Extension, Router,
 };
 use axum_extra::routing::SpaRouter;
+use axum_login::{
+    axum_sessions::{async_session::MemoryStore as SessionMemoryStore, SessionLayer},
+    extractors::AuthContext,
+    secrecy::SecretVec,
+    AuthLayer, AuthUser, RequireAuthorizationLayer, SqliteStore,
+};
 use once_cell::sync::Lazy;
+use sqlx::Pool;
 use tera::Tera;
 use tower_http::catch_panic::CatchPanicLayer;
 
@@ -20,18 +27,53 @@ static TERA: Lazy<Tera> = Lazy::new(|| match Tera::new("templates/**/*") {
     }
 });
 
+#[derive(Debug, Clone, sqlx::FromRow)]
+struct User {
+    id: u32,
+    username: String,
+    password_hash: String,
+    balance: u32,
+}
+
+impl AuthUser<()> for User {
+    fn get_id(&self) -> String {
+        self.id.to_string()
+    }
+
+    fn get_password_hash(&self) -> SecretVec<u8> {
+        SecretVec::new(self.password_hash.clone().into())
+    }
+
+    fn get_role(&self) -> Option<()> {
+        None
+    }
+}
+
+type Auth = AuthContext<User, SqliteStore<User>, ()>;
+
 #[tokio::main]
 async fn main() -> Result<(), impl std::error::Error> {
     //Force initialization in the beginning to ensure all templates parse before
     // opening the server to users
     Lazy::force(&TERA);
+
+    let secret = std::array::from_fn::<u8, 64, _>(|_| fastrand::u8(0..u8::MAX));
+    let session_store = SessionMemoryStore::new();
+    let session_layer = SessionLayer::new(session_store, &secret);
+    let connection = Pool::connect("database.sqlite").await.unwrap();
+    let conn = SqliteStore::<User>::new(connection);
+    let auth_layer = AuthLayer::new(conn, &secret);
+
     let assets = SpaRouter::new("/static", "static");
     let app = Router::new()
         .route("/", get(home))
         .route("/login", get(login_form).post(recieve_login))
         .route("/logout", get(logout))
         .route("/gamble", get(gamble).post(recieve_gamble))
+        .route_layer(RequireAuthorizationLayer::<User, ()>::login())
         .merge(assets)
+        .layer(auth_layer)
+        .layer(session_layer)
         .fallback(error_404)
         .layer(CatchPanicLayer::custom(|_| error_500().into_response()));
 
@@ -49,19 +91,19 @@ async fn login_form() -> impl IntoResponse {
     todo!()
 }
 
-async fn recieve_login(ConnectInfo(who): ConnectInfo<SocketAddr>) {
+async fn recieve_login(mut auth: Auth, ConnectInfo(who): ConnectInfo<SocketAddr>) {
     todo!()
 }
 
-async fn logout(ConnectInfo(who): ConnectInfo<SocketAddr>) {
+async fn logout(mut auth: Auth, ConnectInfo(who): ConnectInfo<SocketAddr>) {
     todo!()
 }
 
-async fn gamble() -> impl IntoResponse {
+async fn gamble(Extension(user): Extension<User>) -> impl IntoResponse {
     todo!()
 }
 
-async fn recieve_gamble() -> impl IntoResponse {
+async fn recieve_gamble(Extension(user): Extension<User>) -> impl IntoResponse {
     todo!()
 }
 
